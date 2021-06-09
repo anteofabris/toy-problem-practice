ce3e4253a0ca5490b7a814db4a293c6b181d70e8
Anteo Fabris <anteo8fabris@gmail.com>
2021-06-09 16:27:08 +0200

    cleaned up pseudocode

--- validPalindromeRemoval.js
+++ validPalindromeRemoval.js
@@ -21,32 +21,3 @@ var validPalindromeRemoval = function (string) {
 var isPal = function (string) {
 return string === string.split('').reverse().join('')
 }
-
-// "abcba", return true
-// "foobof", return true (remove the first 'o', the second 'o', or 'b')
-// "abccab", return false
-
-// var test1 = 'abcba';
-// var test2 = 'foobof';
-// var test3 = 'abccab';
-
-// console.log(validPalindromeRemoval('raicecar'))
-
-
-
-// iterate over input string
-  // take current char out and check if it is a palindrome
-    // if so, return true
-
-// return false
-
-
-  // helper function: isPal(string)
-    // if odd
-      // remove middle char
-    // split string in half
-    // reverse first half
-    // if they match
-      // return true
-
-    // return false
