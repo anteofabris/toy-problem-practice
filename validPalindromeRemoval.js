ba030ffa9159b336dcd343beae17cd3d28b7ed5d
Anteo Fabris <anteo8fabris@gmail.com>
2021-06-09 15:05:34 +0200

    added new problem

---
+++ validPalindromeRemoval.js
@@ -0,0 +1,17 @@
+/* This question is asked by Facebook. Given a string and the ability to delete at most one character, return whether or not it can form a palindrome.
+Note: a palindrome is a sequence of characters that reads the same forwards and backwards.
+
+
+"abcba", return true
+"foobof", return true (remove the first 'o', the second 'o', or 'b')
+"abccab", return false
+*/
+
+// check if it is palindrome
+  // if so, is it odd?
+    // if so, return true
+
+// i - string
+// o - bool
+// c - hopefully not brute force time complexity
+// e - empty string, string has length of one or two
