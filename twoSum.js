fe38c29ccebb133505438b493d387525e06bdf17
Anteo Fabris <anteo8fabris@gmail.com>
2021-06-10 15:52:19 +0200

    added tests

--- twoSum.js
+++ twoSum.js
@@ -10,43 +10,45 @@ Ex: Given the following...
 
 var twoSum = (array, target) => {
 
-for (var i = 0; i < array.length; i ++) {
-  for (var j = 0; j < array.length; j ++) {
-    if (array[j] + array[i] === target && j !== i) {
-      return true;
+  for (var i = 0; i < array.length; i ++) {
+    for (var j = 0; j < array.length; j ++) {
+      if (array[j] + array[i] === target && j !== i) {
+        return true;
+      }
     }
   }
-}
 
-return false;
+  return false;
 
-}
+  }
 
- var test1 = [1, 3, 8, 2];
- var test2 = [3, 9, 13, 7];
- var test3 = [4, 2, 6, 5, 2];
+   var test1 = [1, 3, 8, 2];
+   var test2 = [3, 9, 13, 7];
+   var test3 = [4, 2, 6, 5, 2];
 
- console.log(twoSum(test1))
+   console.log(twoSum(test1, 10))
+   console.log(twoSum(test2, 8))
+   console.log(twoSum(test3, 4))
 
-// i - array of integers, target
-// o - boolean
-// c - hopefully not O(n2)
-// e - array contains less than 2 integers
-
-
-// brute force:
-
-// for each item i in the input array
-  // iterate over the array, skipping that item
-    // if sum of item and second iterated item evaluates to target value
-      // return true
-
-// return false
+  // i - array of integers, target
+  // o - boolean
+  // c - hopefully not O(n2)
+  // e - array contains less than 2 integers
 
 
+  // brute force:
 
-// recursive:
+  // for each item i in the input array
+    // iterate over the array, skipping that item
+      // if sum of item and second iterated item evaluates to target value
+        // return true
 
-// bc:
+  // return false
+
+
+
+  // recursive:
+
+  // bc:
 
 
