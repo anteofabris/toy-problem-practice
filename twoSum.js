cca7419c237528024ffca2ecfa9ae9f75925855b
Anteo Fabris <anteo8fabris@gmail.com>
2021-06-10 15:17:07 +0200

    twoSum first attempt

--- twoSum.js
+++ twoSum.js
@@ -8,3 +8,45 @@ Ex: Given the following...
 [4, 2, 6, 5, 2], k = 4, return true (2 + 2)
 */
 
+var twoSum = (array, target) => {
+
+for (var i = 0; i < array.length; i ++) {
+  for (var j = 0; j < array.length; j ++) {
+    if (array[j] + array[i] === target && j !== i) {
+      return true;
+    }
+  }
+}
+
+return false;
+
+}
+
+ var test1 = [1, 3, 8, 2];
+ var test2 = [3, 9, 13, 7];
+ var test3 = [4, 2, 6, 5, 2];
+
+ console.log(twoSum(test1))
+
+// i - array of integers, target
+// o - boolean
+// c - hopefully not O(n2)
+// e - array contains less than 2 integers
+
+
+// brute force:
+
+// for each item i in the input array
+  // iterate over the array, skipping that item
+    // if sum of item and second iterated item evaluates to target value
+      // return true
+
+// return false
+
+
+
+// recursive:
+
+// bc:
+
+
