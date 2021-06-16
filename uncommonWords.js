e0d4a1c19b3794293f7b687e10b17b3ba53d3fef
Anteo Fabris <anteo8fabris@gmail.com>
2021-06-16 15:22:40 +0200

    solved

--- uncommonWords.js
+++ uncommonWords.js
@@ -8,6 +8,38 @@ sentence1 = "the tortoise beat the haire", sentence2 = "the tortoise lost to the
 sentence1 = "copper coffee pot", sentence2 = "hot coffee pot", return ["copper", "hot"]
 */
 
-var uncommonWords = (arr1, arr2) => {
+var uncommonWords = (str1, str2) => {
+  var ref = {}
+  var deleted = {}
+  var split1 = str1.split(' ')
+  var split2 = str2.split(' ')
+  for (var i = 0; i < split1.length; i ++) {
+    ref[split1[i]] = true
+  }
+  for (var i = 0; i < split2.length; i ++) {
+    if (ref[split2[i]]) {
+      delete ref[split2[i]]
+      deleted[split2[i]] = true
+    } else {
+      if (!deleted[split2[i]]) {
+      ref[split2[i]] = true
+      }
+    }
+  }
+
+  return Object.keys(ref)
 
 }
+
+var sentence1 = "copper coffee pot"
+var sentence2 = "hot coffee pot"
+console.log(uncommonWords(sentence1, sentence2))
+
+// create ref obj
+// iterate over arr1
+  // store each new word in ref
+// iterate over arr2
+  // if exists in ref
+    // delete word
+  // else, add word
+// return Object.keys of ref
