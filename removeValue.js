56618c45b4a7569ef2b66f5e41fd2adbbbd28e5c
Anteo Fabris <anteo8fabris@gmail.com>
2021-06-19 14:31:05 +0200

    pseudocode

--- removeValue.js
+++ removeValue.js
@@ -8,3 +8,7 @@ Ex: Given the following linked lists and values...
 7->12->2->9->null, value = 7, return 12->2->9->null
 */
 
+// iterate through linked list with 2 pointers
+
+// when you find the target
+// set the previous pointer's next value to current.next, thereby removing the node.
