f402ae52944fe0751c978f8432a770ca61df5a70
Anteo Fabris <anteo8fabris@gmail.com>
2021-06-20 15:27:23 +0200

    initial thoughts

--- findMiddleElement.js
+++ findMiddleElement.js
@@ -10,4 +10,42 @@ Ex: Given the following linked lists...
 // i - linked list head
 // o - number, value of middle element
 // c - O(n)
-// e-
+// e - list is even
+// e - list contains only two nodes
+// e - list contains only one node
+
+
+// iterate through linked list and keep track of nodes in a counter
+// if counter is even, iterate up to half of counter plus one
+// if odd, iterate over half of counter rounded up
+
+
+var findMiddleElement = (head) => {
+  var double = true
+  var current = head
+  var half = head
+
+  if (!head) {
+    return null;
+  }
+
+  if (head.next === null) {
+    return head;
+  }
+
+  while (current && current.next) {
+    double = toggleDouble(double);
+    current = current.next
+    if (double) {
+      half = half.next
+    }
+  }
+
+  return half
+
+}
+
+var toggleDouble = (bool) => {
+  if bool return false
+  return true
+}
