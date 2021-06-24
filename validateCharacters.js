02c930f203b0fb7e1c19e1dc3916c4ac7aa5b786
Anteo Fabris <anteo8fabris@gmail.com>
2021-06-24 17:48:19 +0200

    pseudocode

--- validateCharacters.js
+++ validateCharacters.js
@@ -10,4 +10,18 @@ Ex: Given the following strings...
 
 var validateCharacters = (string) => {
 
+
 }
+
+// create a stack array
+// create a ref obj with opening braces as keys and closing braces as valies
+
+// iterate over string
+  // if current char exists as key in ref aka it is an opening brace
+    // push its value (aka the respective closing brace) to the stack
+  // else (means it is a closing brace)
+    // if current char is not equal to the last value popped off of stack
+      // it's an invalid parens arrangement
+
+// if all opening and closing braces match and in the correct order, the stack should be empty by now, so:
+// return whether stack.length is 0
