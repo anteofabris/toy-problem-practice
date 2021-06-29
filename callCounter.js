b9a387c7057117e5db60cf845f09ea655fab0995
Anteo Fabris <anteo8fabris@gmail.com>
2021-06-29 15:06:13 +0200

    movingAverage

--- callCounter.js
+++ callCounter.js
@@ -1,5 +1,6 @@
 /*
-This question is asked by Google. Create a class CallCounter that tracks the number of calls a client has made within the last 3 seconds. Your class should contain one method, ping(int t) that receives the current timestamp (in milliseconds) of a new call being made and returns the number of calls made within the last 3 seconds.
+This question is asked by Google. Create a class CallCounter that tracks the number of calls a client has made within the last 3 seconds.
+  Your class should contain one method, ping(int t) that receives the current timestamp (in milliseconds) of a new call being made and returns the number of calls made within the last 3 seconds.
 Note: you may assume that the time associated with each subsequent call to ping is strictly increasing.
 
 Ex: Given the following calls to ping…
@@ -10,3 +11,8 @@ ping(3000), return 3 (3 calls within the last 3 seconds)
 ping(3002), return 3 (3 calls within the last 3 seconds)
 ping(7000), return 1 (1 call within the last 3 seconds)
 */
+
+
+var ping = function () => {
+
+}
