84ef92764aa1a079bf0957e20dd004eb022497f8
Anteo Fabris <anteo8fabris@gmail.com>
2021-06-30 15:13:38 +0200

    initial setup

--- queueStack.js
+++ queueStack.js
@@ -6,3 +6,23 @@ pop() (removing an item),
 peek() (returning the top value without removing it), and
 empty() (whether or not the stack is empty).
 */
+
+class Queue {
+  this.stack = []
+}
+
+Queue.prototype.push = (n) => {
+
+}
+
+Queue.prototype.pop = () => {
+
+}
+
+Queue.prototype.peek = () => {
+
+}
+
+Queue.prototype.empty = () => {
+  return this.stack = []
+}
