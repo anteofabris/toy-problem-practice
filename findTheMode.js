8eb675716fa31034caec77837b329f38d33d79a7
Anteo Fabris <anteo8fabris@gmail.com>
2021-07-07 15:31:13 +0200

    pseudocode

--- findTheMode.js
+++ findTheMode.js
@@ -20,3 +20,50 @@ Ex: Given the following tree...
 return 9.
 */
 
+
+var findTheMode = function (root) => {
+
+}
+
+// create an object to store mode and its occurrence count - set to node
+// create countCurrent var set to 0
+
+// iterate over bst
+  // if node has a left child
+    // if left child is equal to node
+      // current = node
+      // while current.left val is equal to current.val
+        // countCurrent ++
+        // current = current.left
+      // if countCurrent > ref[most]
+        // delete ref[most]
+        // most = current.val
+        // ref[most] = currentCount
+    // if left child is smaller
+      // search(left)
+    // if node has a right child
+      // if right child is equal to node
+        // current = node
+        // while current.right equals current.val
+          // countCurrent ++
+          // current = current.right
+        // if countCurrent > ref[most]
+          // delete ref[most]
+          // most = current.val
+          // ref[most] = currentCount
+        // if right child is greater
+          // search(right)
+  // if no children
+    // return ref
+
+
+// var most = root
+// if left exists
+  //
+
+
+// function dfs takes root
+
+  // visit node
+  // dfs(left)
+  // dfs(right)
